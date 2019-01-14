8aa736a8109e250adf589112c1a9e9fe4335dcb8
Louis Sanchez <louis.sa@outlook.com>
2019-01-14 10:14:16 -0500

    fixed SEO content

--- config/website.js
+++ config/website.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  siteTitle: 'LsMagic', // Navigation and Site Title
-  siteTitleAlt: 'Emma - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-emma.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'Louis Sanchez', // Navigation and Site Title
+  siteTitleAlt: 'Louis Sanchez - Portfolio', // Alternative Site title for SEO
+  siteUrl: 'lsmagic.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
+  siteDescription: 'Louis Sanchez - Web Developer, Designer, Artist',
   author: 'Louis Sanchez', // Author for schemaORGJSONLD
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@lsmagic', // Twitter Username
-  ogSiteName: 'emma', // Facebook Site Name
+  ogSiteName: 'lsmagic', // Facebook Site Name
   ogLanguage: 'en_US',
-  googleAnalyticsID: 'UA-12345689-1',
+  googleAnalyticsID: 'UA-132416990-1',
   // Manifest and Progress color
   themeColor: '#42D3B6',
   backgroundColor: '#2b2e3c',
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -20,10 +20,13 @@ const About = () => (
       </ImgWrapper>
       <h1>Hi!</h1>
       <p>
-        My name is Louis Sanchez and I am a designer, developer, and artist living in New York.
-        I have a degree in Computer Science and am currently working at St. George's University Support
-        Services. I have a proven history of using scripting and automation to streamline processes and
-        I dabble in various freelance design work.
+        My name is Louis Sanchez and I am a designer, developer, and artist
+        living in New York. I have a degree in Computer Science and am currently
+        working at St. George's University Support Services. I have a proven
+        history of using scripting and automation to streamline processes and I
+        love experimenting with new technologies and techniques. I am very
+        passionate about web development and I am constantly tackling new
+        problems and expanding my skillset.
       </p>
     </Container>
   </Layout>
